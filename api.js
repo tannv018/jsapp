dfacbee56fb88f7ba92076d6fd949ae3bea5b89d
tannv <tannv99@gmail.com>
2024-11-20 14:04:26 +0700

    add code api.js

--- api.js
+++ api.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3000, ()=>{
-    console.log("Sever is now listening at port 3000");
+app.listen(3001, ()=>{
+    console.log("Sever is now listening at port 3001");
 })
 
 client.connect();
@@ -28,6 +28,35 @@ app.get('/todos/:id', (req, res)=>{
     });
     client.end;
 })
-client.connect();
 
 
+app.post('/todos', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `insert into todos(todo, status) 
+                       values('${user.todo}', '${user.status}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+
+app.put('/todos/:id', (req, res)=> {
+    let user = req.body;
+    let updateQuery = `update users
+                       set  = todo'${user.todo}',
+                       status = '${user.status}',
+                       where id = ${user.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
